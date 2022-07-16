21fc9db28faff2a177f5c7ee1d7b0086d34eb057
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-07-16 20:23:32 +0300

    database settings for event model

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'budget' => $this->faker->randomFloat(2,100,50000),
+            'date' => $this->faker->unixTime()
+        ];
+    }
+}
